8b3dbc894a756a020a965ee1972d7bb5d7ee888d
Gordon Martin <gordonhughmartin@gmail.com>
2013-05-01 13:00:07 +0100

    Using iteratee and FSM in IRCCLient. Need to implement some parsing of the message.

--- src/main/scala/IrcClient.scala
+++ src/main/scala/IrcClient.scala
@@ -3,6 +3,7 @@ import akka.actor.Actor
 import akka.actor.IO
 import akka.actor.IOManager
 import akka.util.ByteString
+import akka.actor.FSM
 
 case class IRCMessage(user: String, channel: String, message: String)
 
@@ -13,24 +14,79 @@ case object Disconnected extends ClientState
 sealed trait ClientData
 case class ResponsableFor(server: Server) extends ClientData
 
-class IRCClient(server: Server, ircManager: IRCManager) extends Actor {
+class IRCClient(server: Server, ircManager: IRCManager) extends Actor with FSM[ClientState, ClientData] {
 
+  val state = IO.IterateeRef.Map.async[IO.Handle]()(context.dispatcher)
   val ioManager = IOManager(context.system)
-
   val socket = ioManager.connect(server.address, server.port)
 
-  def receive = {
+  startWith(Disconnected, ResponsableFor(server))
 
-    case IO.Connected(socket, address) =>
+  when(Disconnected) {
+
+    case Event(IO.Connected(socket, address), server: ResponsableFor) =>
+      state(socket) flatMap (_ => IRCClient.processMessage(socket))
       println("Successfully connected to " + address)
+      goto(Connected) using server
+  }
 
-    case IO.Closed(socket: IO.SocketHandle, cause) =>
+  when(Connected) {
+
+    case Event(IO.Read(socket, bytes), server: ResponsableFor) =>
+      state(socket)(IO Chunk bytes)
+      stay using server
+
+    case Event(IO.Closed(socket: IO.SocketHandle, cause), server: ResponsableFor) =>
+      //@TODO: Attempt to reconnect => Using a timer?
+      state(socket)(IO EOF)
+      state -= socket
       println("Socket has closed, cause: " + cause)
-
-    case IO.Read(socket, bytes) => println(ascii(bytes))
+      goto(Disconnected) using server
 
   }
 
+  /*
+  def receive = {
+
+    case IO.Connected(socket, address) =>
+      state(socket) flatMap (_ => IRCClient.processMessage(socket))
+      println("Successfully connected to " + address)
+
+    case IO.Closed(socket: IO.SocketHandle, cause) =>
+      //@TODO: Attempt to reconnect
+      state(socket)(IO EOF)
+      state -= socket
+      println("Socket has closed, cause: " + cause)
+
+    case IO.Read(socket, bytes) =>
+      state(socket)(IO Chunk bytes)
+
+  } */
+
+}
+
+object IRCMessageParser {
   def ascii(bytes: ByteString): String = bytes.decodeString("US-ASCII").trim
+
+  def parse(bytes: ByteString): IRCMessage = {
+    ???
+
+  }
+}
+
+object IRCClient {
+
+  def processMessage(socket: IO.SocketHandle): IO.Iteratee[Unit] = {
+    IO repeat {
+      for {
+        line <- IO takeUntil ByteString("\r\n")
+      } yield {
+        val theLine = line
+        println(IRCMessageParser.ascii(theLine))
+      }
+
+    }
+
+  }
 
 }
